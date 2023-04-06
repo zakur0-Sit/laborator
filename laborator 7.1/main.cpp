0403e60d4c320c39f338ce5814879fe36b89668f
zakur0-Sit <cazacioc51@gmail.com>
2023-04-06 18:55:58 +0300

    good

--- laborator 7.1/main.cpp
+++ laborator 7.1/main.cpp
@@ -3,12 +3,12 @@ using namespace std;
 
 float operator ""_Kelvin(long double temp)
 {
-    return static_cast<float>(temp) + 273.15f;
+    return static_cast<float>(temp) - 273.15f;
 }
 
 float operator ""_Fahrenheit(long double temp)
 {
-    return static_cast<float>(temp *9/5+32);
+    return static_cast<float>((temp - 32)*5/9);
 }
 
 int main() {
